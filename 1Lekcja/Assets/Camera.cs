95658d69b5a49133dfa1e24f91b31845f64dba7c
Szcurek <bartektyszcz@gmail.com>
2021-10-05 18:57:27 +0200

    Lekcja 1

---
+++ 1Lekcja/Assets/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Transform Objektobserwowany;
+    Vector3 odleglosc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        odleglosc = transform.position - Objektobserwowany.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Objektobserwowany.position + odleglosc;
+        transform.LookAt(Objektobserwowany);
+    }
+}
---
+++ 1Lekcja/Assets/Wheel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+
+    public WheelCollider wh;
+    public Transform wheel_model;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos;
+        Quaternion rot;
+
+        wh.GetWorldPose(out pos, out rot);
+        wheel_model.position = pos;
+        wheel_model.rotation = rot;
+    }
+}
